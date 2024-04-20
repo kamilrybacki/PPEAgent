f3e496268147588233b8441eee4f1d5d6e7a5cb7
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-04-20 22:48:57 +0200

    Refactor get_root_path function to return HTML response in general.py

--- src/agent/routers/general.py
+++ src/agent/routers/general.py
@@ -6,6 +6,15 @@ GENERAL_ROUTER = fastapi.APIRouter()
 
 @GENERAL_ROUTER.get('/')
 async def get_root_path() -> fastapi.responses.Response:
-    return fastapi.responses.JSONResponse({
-        'Hello': 'World'
-    })
+    return fastapi.responses.HTMLResponse(
+        content='''
+            <html>
+                <head>
+                    <title>PPE Agent</title>
+                </head>
+                <body>
+                    <h1>Welcome to PPE Agent</h1>
+                </body>
+            </html>
+        '''
+    )
